b830dd55dc39b19e3320407c7f5f5d8f9cd516b0
Егоров Константин <konstantin_egorov_06@mail.ru>
2025-09-19 18:19:38 +0300

    refactor: extra code removed

--- ISRPO_1/MainWindow.xaml.cs
+++ ISRPO_1/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace ISRPO_1
 {
     public partial class MainWindow : Window
     {
-        private double[,] _matrix;
+        private double[,] matrix;
 
         public MainWindow()
         {
@@ -23,71 +23,43 @@ namespace ISRPO_1
                 return;
             }
 
-            // Создаём матрицу со случайными числами, округлёнными до десятых
-            _matrix = new double[n, m];
+            matrix = new double[n, m];
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
-                    // 20% шанс поставить 0.0
                     if (rnd.NextDouble() < 0.2)
                     {
-                        _matrix[i, j] = 0.0;
+                        matrix[i, j] = 0.0;
                     }
                     else
                     {
-                        // Генерируем число от -5.0 до 5.0 и округляем до десятых
-                        _matrix[i, j] = Math.Round(rnd.NextDouble() * 10 - 5, 1);
+                        matrix[i, j] = Math.Round(rnd.NextDouble() * 10, 1);
                     }
                 }
             }
-
-            // Привязываем к DataGrid через DataTable
-            DgMatrix.ItemsSource = _matrix.ToDataTableTwo().DefaultView;
+            DgMatrix.ItemsSource = matrix.ToDataTableTwo().DefaultView;
         }
 
         private void BtnCalculate_Click(object sender, RoutedEventArgs e)
         {
-            if (_matrix == null)
+            if (matrix == null)
             {
                 MessageBox.Show("Сначала создайте матрицу.");
                 return;
             }
 
-            int n = _matrix.GetLength(0);
-            int m = _matrix.GetLength(1);
-
-            // Обновляем матрицу из DataGrid (на случай, если пользователь что-то изменил)
-            var view = (DataView)DgMatrix.ItemsSource;
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    var cellValue = view[i][j];
-                    if (cellValue != DBNull.Value && double.TryParse(cellValue.ToString(), out double val))
-                    {
-                        _matrix[i, j] = val;
-                    }
-                    else
-                    {
-                        MessageBox.Show($"Ошибка в ячейке [{i + 1}, {j + 1}]. Введите корректное число.");
-                        return;
-                    }
-                }
-            }
-
-            // Вычисляем сумму столбцов, содержащих хотя бы один ноль
             double totalSum = 0;
 
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 bool hasZero = false;
                 double columnSum = 0;
 
-                for (int i = 0; i < n; i++)
+                for (int i = 0; i < matrix.GetLength(0); i++)
                 {
-                    columnSum += _matrix[i, j];
-                    if (_matrix[i, j] == 0)
+                    columnSum += matrix[i, j];
+                    if (matrix[i, j] == 0)
                         hasZero = true;
                 }
 
